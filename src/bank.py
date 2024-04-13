5c365cbd4d58a43977d752bd3cdb24a44839a888
Noah Syrkis <noah@syrkis.com>
2024-04-13 11:03:20 -0300

    parse BT DSL

--- src/bank.py
+++ src/bank.py
@@ -5,13 +5,45 @@
 # imports
 from lark import Lark
 import yaml
-
-# load lark grammar from ../grammar.lark
-with open("grammar.lark", "r") as f:
-    grammar = f.read()
-    parser = Lark(grammar, start="tree")
+import json
 
 
-example = "tree ( sequence ( atomic ( noah ), atomic ( syrkis)))"
+# constants
+ATOMICS = ["enemy_found", "find_enemy", "attack_enemy", "not_fn"]
 
-print(parser.parse(example))  # tree { sequence { } }
+
+# functions
+def grammar_fn():
+    with open("grammar.lark", "r") as f:
+        return Lark(f.read(), start="node")
+
+
+def parse_fn(fname):
+    with open(fname, "r") as f:
+        return grammar_fn().parse(f.read())
+
+
+def dict_fn(tree):
+    if tree.data.title() == "String":
+        return tree.children[0]
+    elif tree.data.title() == "Node":
+        return dict_fn(tree.children[0])
+    elif tree.data.title() == "Nodes":
+        return [dict_fn(child) for child in tree.children]
+    elif tree.data.title() in ["Action", "Condition"]:
+        return {tree.data.title().lower(): dict_fn(tree.children[0])}
+    else:  # Sequence or Fallback or Decorator
+        key = tree.data.title().lower()
+        value = [dict_fn(child) for child in tree.children]
+        return {key: value if len(value) > 1 else value[0]}
+
+
+def main():
+    tree = parse_fn("bank/ally.bt")
+    dict_tree = dict_fn(tree)
+    json_tree = json.dumps(dict_tree, indent=2)
+    print(json_tree)
+
+
+if __name__ == "__main__":
+    main()
