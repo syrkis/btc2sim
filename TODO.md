c6d1a7da4c27d97fa4619365dae98cb9edf2a0d6
Noah Syrkis <noah@syrkis.com>
2024-05-27 22:09:51 -0300

    plot metrics of sims

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# TODO
+
+- [ ] Auto beautify tree. 2024-05-30
+- [ ] Speedy playback animations (nice to have).
+- [ ] Attach LMM to website.
+- [ ] Move from for loop to `jax.lax.scan`. 2024-05-30
+- [ ] Select BT by hovering through MAP-Elites?
+
+# DONE
+
+- [x] Compile tree from input. 2024-05-27
+- [x] Compute metrics live on website. 2024-05-27
+- [x] Ensure streamlit app runs with jaxmarl.
--- app.py
+++ app.py
@@ -4,11 +4,13 @@
 
 # imports
 import streamlit as st
+from matplotlib import pyplot as plt
 
 from jaxmarl import make
 from jaxmarl.environments.smax import map_name_to_scenario
 from jax import jit, vmap, random
 import jax
+import jax.numpy as jnp
 
 
 from src.utils import scenarios
@@ -18,10 +20,13 @@ from src.bt import make_bt
 # constants
 grammar = "grammar.lark"
 page_title = "c2sims | meta gaming platform"
+n_envs_sqrt = 3
+n_envs = n_envs_sqrt**2
 
 # set page config
 conf = dict(initial_sidebar_state="expanded", layout="wide", page_title=page_title)
 st.set_page_config(**conf)
+plt.style.use("dark_background")
 
 
 ##################
@@ -43,7 +48,7 @@ def main():
     tree = domain_langauge_fn(top_cols[1], tree_bank)
 
     # simulation
-    rng = random.PRNGKey(0)
+    rng = random.PRNGKey(4)
     seqs = simulate_fn(rng, tree, scenario)
 
     # outputs
@@ -59,13 +64,13 @@ def main():
 def simulate_fn(rng, bt, scenario):
     # simulate the behavior tree
     rng, key = random.split(rng)
-    reset_keys = random.split(key, 6)
+    reset_keys = random.split(key, n_envs)
     env = make("SMAX", scenario=map_name_to_scenario(scenario))
     obs, state = vmap(env.reset)(reset_keys)
     seqs = []
-    for i in range(10):  # could replace with a lax.scan for speed
+    for i in range(15):  # could replace with a lax.scan for speed
         rng, key = random.split(rng)
-        step_keys = random.split(key, 6)
+        step_keys = random.split(key, n_envs)
         action = {a: bt(state, obs[a], a, env)[1] for a in env.agents}
         obs, state, reward, done, info = vmap(env.step)(step_keys, state, action)
         seqs.append((obs, state, reward, done, info))
@@ -73,9 +78,39 @@ def simulate_fn(rng, bt, scenario):
 
 
 def metrics_fn(col, seqs):
-    col.write("Metrics")
+    col.write("Returns")
     returns = [seq[2] for seq in seqs]
-    col.write(returns)
+    fig, axes = plt.subplots(n_envs_sqrt, n_envs_sqrt, figsize=(9, 9))
+    for idx, ax in enumerate(axes.flatten()):
+        ally_rets = []
+        enemy_rets = []
+        for ret in returns:
+            ally_ret = sum([v[idx] for k, v in ret.items() if k.startswith("ally")])
+            enemy_ret = sum([v[idx] for k, v in ret.items() if k.startswith("enemy")])
+            ally_rets.append(ally_ret)
+            enemy_rets.append(enemy_ret)
+
+        ax.plot(
+            range(len(returns)),
+            jnp.cumsum(jnp.array(ally_rets)),
+            color="blue",
+            label="Ally",
+        )
+        ax.plot(
+            range(len(returns)),
+            jnp.cumsum(jnp.array(enemy_rets)),
+            color="red",
+            label="Enemy",
+        )
+        # hide axis labels and ticks
+        ax.set_xticks([])
+        ax.set_yticks([])
+        # set title
+        ax.set_title(f"Env {idx}")
+        if idx == 0:
+            ax.legend()
+    plt.tight_layout()
+    col.pyplot(fig)
     return returns
 
 
@@ -108,13 +143,13 @@ def domain_langauge_fn(col, tree_bank):
     tree = vmap(make_bt(dict_fn(tree_dict)), in_axes=(0, 0, None, None))
     # tree = vmap(tree_bank[0]["tree"], in_axes=(0, 0, None, None))
     # pretty print string of tree
-    return tree
+    return jit(tree, static_argnums=(2, 3))
 
 
 def sidebar_fn():
     st.sidebar.title("Settings")
     st.sidebar.write("Choose a scenario")
-    scenario = st.sidebar.selectbox("", scenarios)
+    scenario = st.sidebar.selectbox("", scenarios[::-1])
     return scenario
 
 
