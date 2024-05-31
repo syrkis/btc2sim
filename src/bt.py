218fe43dff16fa3b957f258728163b2aad782a30
Timothee-ANNE <timonathan@gmail.com>
2024-05-31 17:26:01 +0200

    activation of bts

--- src/bt.py
+++ src/bt.py
@@ -26,48 +26,53 @@ PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
 def tree_fn(children: List[NF], node_kind: bool) -> NF:  # sequence / fallback
     # run parallel trees
     def tick(state, obs: jnp.array, agent, env) -> Status:
-        # idxs describes which of the trees should run
-        status, action, on = (S if node_kind else F, STAND, True)  # on=need act?
+        status, action, on, selected_node_id = (S if node_kind else F, STAND, True, -1)  # on=need act?
         for child in children:  # loop through all children
-            ns, na = child(state, obs, agent, env)  # new state and action
+            ns, na, node_id = child(state, obs, agent, env)  # new state and action
             flag = jnp.logical_and(jnp.where(node_kind, ns != S, ns != F), on)
-            # on, status, action = jnp.where(flag, (0, ns, na), (on, status, action))
             on = jnp.where(flag, 0, on)
             status = jnp.where(flag, ns, status)
             action = jnp.where(flag, na, action)
-        return status, action
+            selected_node_id = jnp.where(flag, node_id, selected_node_id)
+        return status, action, selected_node_id
 
     return tick
 
 
-def atomic_fn(fn: Callable, dec_fn: Callable = None) -> NF:
+def atomic_fn(fn: Callable, node_id, dec_fn: Callable = None) -> NF:
     def tick(state, obs: jnp.array, agent, env) -> Status:
         args = (state, obs, agent, env)
         fn_res = fn(*args)
         res = dec_fn(*fn_res) if dec_fn is not None else fn_res
-        out = res if isinstance(res, tuple) else (res, STAND)
-        return out
+        state, action = res if isinstance(res, tuple) else (res, STAND)
+        return state, action, node_id
 
     return tick
 
 
 def make_bt(tree) -> NF:
-    def make_node(node: dict) -> NF:
+    node_id = 0
+    
+    def make_node(node: dict, node_id: int) -> NF:
         if node[0] in ["sequence", "fallback"]:
-            children = [make_node(child) for child in node[1]]
-            return tree_fn(children, node[0] == "sequence")
+            node_id += 1
+            children = []
+            for child in node[1]:
+                child_node, node_id = make_node(child, node_id)
+                children.append(child_node)
+            return tree_fn(children, node[0] == "sequence"), node_id
         if node[0] in ["condition", "action"]:
             _, func, args = node[0], node[1][0], node[1][1]
             args = [args] if isinstance(args, str) else args
             fn = ATOMIC_FNS[func] if len(args) == 0 else ATOMIC_FNS[func](*args)
-            return atomic_fn(fn)
+            return atomic_fn(fn, node_id), node_id+1
         if node[0] == "decorator":
             dec_fn = ATOMIC_FNS[node[1][0]]
-            subtree = make_node(node[1][1])
-            return atomic_fn(subtree, dec_fn)
+            subtree, node_id = make_node(node[1][1], node_id)
+            return atomic_fn(subtree, node_id, dec_fn), node_id+1
         raise ValueError(f"Invalid node type: {node}")
-
-    return partial(make_node(tree))  # partial to pass env to all nodes
+    tree, _ = make_node(tree, node_id)
+    return partial(tree)  # partial to pass env to all nodes
 
 
 def main():
