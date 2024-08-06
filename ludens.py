0f0e8120e3251605d8c95dfbf024579acc29df8d
Noah Syrkis <noah@syrkis.com>
2024-08-06 14:46:32 +0200

    switch and map bts

--- ludens.py
+++ ludens.py
@@ -1,14 +1,17 @@
 # %% Imports
 import os
+from tqdm import tqdm
+from functools import partial
 
-from jax._src import tree_util
-# os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=5'
-import c2sim
-import parabellum as pb
+# os.environ["XLA_FLAGS"] = '--xla_force_host_platform_device_count=5'  # this for runnning on multiple devices with pmap
+
 import jax
 import jax.numpy as jnp
-from jax import random, vmap, jit, pmap
-from tqdm import tqdm
+from jax import random, vmap, jit, pmap, tree_util
+
+import parabellum as pb
+import c2sim
+
 
 # %% Constants
 places = ['Vesterbro, København, Denmark', 'Nørrebro, København, Denmark']
@@ -28,14 +31,6 @@ def bts_fn(bt_strs):  # <- use switch to select bt when running (combine with fo
     bts = [vmap(c2sim.bt.seed_fn(dsl_tree), in_axes=(0, 0, None, None)) for dsl_tree in dsl_trees]
     return bts
 
-# %%
-def acts_fn(obs, env_info, agent_info):  # take actions for all agents in parallel
-    idxs = jnp.repeat(jnp.arange(env.num_agents), parallel_envs)  # this can be done outside of the function only once
-    batched_obs = batchify(obs)
-    batched_acts = bt(batched_obs, idxs, env_info, agent_info)[1].astype(jnp.int32)  # first value is the state, second is the action
-    acts = unbatchify(batched_acts, idxs, obs)
-    return acts
-
 
 # %%
 def batchify(obs):
@@ -50,18 +45,36 @@ def unbatchify(batched_acts, idxs, obs):
 
 
 # %%
+def bt_fn(bt, batched_obs, env_info, agent_info):  # take actions for all agents in parallel
+    batched_acts = bt(batched_obs, idxs, env_info, agent_info)[1].astype(jnp.int32)
+    return batched_acts
+    # acts = unbatchify(batched_acts, idxs, obs)
+    # return acts
+
+
+
+# %%
 envs, bts = envs_fn(places), bts_fn(bt_strs)
+bt_fns = [partial(bt_fn, bt) for bt in bts]
 rngs = random.split(random.PRNGKey(0), len(envs) * parallel_envs).reshape(len(envs), parallel_envs, 2)
 
 # %%
+fns = [env.step for env in envs]
 for rng, env in zip(rngs, envs):  # <- replace with fori_loop and switch
-    obs, state = vmap(env.reset)(rng)
+    idxs = jnp.repeat(jnp.arange(env.num_agents), parallel_envs)
     env_info = c2sim.info.env_info_fn(env)
     agent_info = c2sim.info.agent_info_fn(env)
-    for j, bt in enumerate(bts):  # <- replace with fori_loop and switch (use pmap)
-        for _ in range(100):  # <- replace with scan though rngs
-            acts = acts_fn(obs, env_info, agent_info)
-            obs, state, _, _, _= vmap(env.step)(rng, state, acts)
+    obs, state = vmap(env.reset)(rng)
+    for i in range(100):  # <- replace with scan though rngs
+        batched_obs = batchify(obs)
+        batched_acts = jax.lax.map(lambda i: jax.lax.switch(i, bt_fns, batched_obs, env_info, agent_info), jnp.arange(len(bts)))
+        acts = vmap(unbatchify, in_axes=(0, None, None))(batched_acts, idxs, obs)
+        # obs, state, *_ = jax.lax.map(vmap(env.step))(rng, state, acts)
+        print(batched_acts, acts)
+        exit()
+        # for bt in bts:  # replace with fori_loop and switch
+            # obs, state = for_fn(bt, env, rng, obs, state, env_info, agent_info)
+            # bt_fn(bt, env, rng, obs, state, env_info, agent_info)
         break
     break
 
