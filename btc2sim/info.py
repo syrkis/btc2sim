1ad9cc434f6661b1b39ef9e7e93726b3fb527f6e
Noah Syrkis <noah@syrkis.com>
2024-08-14 12:55:51 +0200

    refactor btc2sim

--- btc2sim/info.py
+++ btc2sim/info.py
@@ -5,7 +5,7 @@
 # %%  imports
 import jax.numpy as jnp
 from jax import random, vmap, jit
-import c2sim
+import btc2sim
 
 
 # %% functions
@@ -14,30 +14,31 @@ def info_fn(env, parallel_envs):
     agent_info = agent_info_fn(env)
 
 
-
 def env_info_fn(env):
     return c2sim.types.EnvInfo(
         num_agents=env.num_agents,
         num_allies=env.num_allies,
         num_enemies=env.num_enemies,
-
         num_types=jnp.array(len(env.unit_type_names)),
         num_own_features=jnp.array(len(env.own_features)),
-
         world_steps_per_env_step=env.world_steps_per_env_step,
         time_per_step=env.time_per_step,
-
         # map info
         map_width=env.map_width,
         map_height=env.map_height,
-        terrain_raster = env.terrain_raster, # 2D array of terrain types
+        terrain_raster=env.terrain_raster,  # 2D array of terrain types
     )
 
+
 def agent_info_fn(env):
-    agent_info = {a: c2sim.types.AgentInfo(
-        agent_id=env.agent_ids[a],
-        velocity=env.unit_type_velocities[env.agent_ids[a]],
-        sight_range=env.unit_type_sight_ranges[env.agent_ids[a]],
-        attack_range=env.unit_type_attack_ranges[env.agent_ids[a]],
-        is_ally=a.startswith('ally')) for a in env.agents}
+    agent_info = {
+        a: c2sim.types.AgentInfo(
+            agent_id=env.agent_ids[a],
+            velocity=env.unit_type_velocities[env.agent_ids[a]],
+            sight_range=env.unit_type_sight_ranges[env.agent_ids[a]],
+            attack_range=env.unit_type_attack_ranges[env.agent_ids[a]],
+            is_ally=a.startswith("ally"),
+        )
+        for a in env.agents
+    }
     return agent_info
