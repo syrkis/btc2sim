b3db5d9ab52f6165fe0e7b62454cb93bb51bcbf3
Timothee-ANNE <timonathan@gmail.com>
2024-10-28 15:58:17 +0100

    agent_info array like for vmap

--- btc2sim/info.py
+++ btc2sim/info.py
@@ -39,8 +39,30 @@ def agent_info_fn(env, direction_maps={}):
             sight_range=env.unit_type_sight_ranges[env.scenario.unit_types[env.agent_ids[a]]],
             attack_range=env.unit_type_attack_ranges[env.scenario.unit_types[env.agent_ids[a]]],
             is_ally=a.startswith("ally"),
-            direction_map=jnp.ones(env.terrain.building.shape, dtype=jnp.int32) * 4 if a not in direction_maps else direction_maps[a],  # 4 is for the standing action
+            direction_map=jnp.ones(env.terrain.building.shape, dtype=jnp.int32) if a not in direction_maps else direction_maps[a],
         )
         for a in env.agents
     }
     return agent_info
+
+def agent_info_fn_to_vmap(env, direction_maps={}):
+    agent_info = btc2sim.classes.AgentInfo(
+            agent_id=jnp.array([env.agent_ids[a] for a in env.agents]),
+            velocity=jnp.array([env.unit_type_velocities[env.scenario.unit_types[env.agent_ids[a]]] for a in env.agents]),
+            sight_range=jnp.array([env.unit_type_sight_ranges[env.scenario.unit_types[env.agent_ids[a]]] for a in env.agents]),
+            attack_range=jnp.array([env.unit_type_attack_ranges[env.scenario.unit_types[env.agent_ids[a]]] for a in env.agents]),
+            is_ally=jnp.array([a.startswith("ally") for a in env.agents]),
+            direction_map=jnp.array([jnp.ones(env.terrain.building.shape, dtype=jnp.int32) if a not in direction_maps else jnp.array(direction_maps[a]) for a in env.agents]),
+        )
+    return agent_info
+
+def split_agent_info(agent_info, indices):
+    new_agent_info = btc2sim.classes.AgentInfo(
+        agent_id=agent_info.agent_id[indices],
+        velocity=agent_info.velocity[indices],
+        sight_range=agent_info.sight_range[indices],
+        attack_range=agent_info.attack_range[indices],
+        is_ally=agent_info.is_ally[indices],
+        direction_map=agent_info.direction_map[indices],
+    )
+    return new_agent_info
