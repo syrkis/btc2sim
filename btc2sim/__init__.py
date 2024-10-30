bbbe6e6132c71b25e1b354b104906ca203e68825
Noah Syrkis <noah@syrkis.com>
2024-10-30 09:29:57 +0100

    tim

--- btc2sim/__init__.py
+++ btc2sim/__init__.py
@@ -4,6 +4,7 @@ from btc2sim import bt
 from btc2sim import utils
 from btc2sim import classes
 from btc2sim import info
+from btc2sim import terrain_db
 
 # exports
-__all__ = ["dsl", "bt", "utils", "classes", "info"]
+__all__ = ["dsl", "bt", "utils", "classes", "info", "terrain_db"]
