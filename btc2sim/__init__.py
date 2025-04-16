c5293523168b16b314ecd92e1539e357e03f5c62
Noah Syrkis <noah@syrkis.com>
2025-04-16 18:11:25 +0200

    refactor bts away into act

--- btc2sim/__init__.py
+++ btc2sim/__init__.py
@@ -1,9 +1,8 @@
 # imports
 from btc2sim import dsl
 from btc2sim import act
-from btc2sim import bts
 from btc2sim import utils
 from btc2sim import types
 
 # exports
-__all__ = ["act", "dsl", "bts", "utils", "types"]
+__all__ = ["act", "dsl", "utils", "types"]
